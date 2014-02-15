b72d8bce865d10d8356b05db5342a0599b2594a2
ychartois <yannickchartois@wanadoo.fr>
2014-02-15 17:10:14 +0100

    step 2

--- ng-darrrt-codelab/web/1-blankbadge/piratebadge.dart
+++ ng-darrrt-codelab/web/1-blankbadge/piratebadge.dart
@@ -2,6 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:angular/angular.dart';
+
+@MirrorsUsed(override: '*')
+import 'dart:mirrors';
+
 void main() {
-  // Your app starts here.
+  ngBootstrap();
 }
